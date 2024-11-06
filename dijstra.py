1ee7c9a6d04c1ae8b373480072455edc5831294b
vegar <vegar1201@live.no>
2024-11-06 16:39:22 +0100

    Init

---
+++ dijstra.py
@@ -0,0 +1,113 @@
+
+from grid import Grid2D
+import heapq
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+# class Graph:
+#
+#     def __init__(self, grid) -> None:
+#         self.grid = grid
+        
+
+# class Node:
+#     def __init__(self, pos=(None,None), dist=None) -> None:
+#         self.id = str(pos)
+#         self.pos = pos
+#         self.dist = dist
+
+
+class Dikstras:
+
+    def __init__(self, grid: Grid2D, start_edge: tuple[int,int], end_edge: tuple[int,int]) -> None:
+        self.grid = grid
+        self.start_edge = start_edge
+        self.end_edge = end_edge
+
+        self.dist_shape = (grid.xEdgeRange.max()+1, grid.yEdgeRange.max()+1)
+        self.distances = np.full(self.dist_shape, np.inf)
+        self.distances[start_edge] = 0
+
+        self.parents_ = {
+            self.calcualte_pos_id(start_edge): (-1,-1)
+        }
+
+        self.queue_ = []
+        heapq.heappush(self.queue_, (0, start_edge))
+
+        # self.directions_ = [(0,1), (0, -1), (1, 0), (-1, 0)] # FIXME: This should also go on diagonal
+
+    def calcualte_pos_id(self, edge: tuple)->str:
+        return str( edge[0] + edge[1] * self.dist_shape[1] )
+
+    def check_edge_feasiable(self, edge):
+        pass
+
+    def iter(self) -> tuple[bool, list]:
+
+        dist, edge = heapq.heappop(self.queue_)
+        print(f"current edge {edge}, with dist {dist}")
+
+        if edge == self.end_edge:
+            print("found path")
+            return True, self.reconstructShortestPath(edge)
+
+        if dist > self.distances[edge]:
+            # FIXME: can we return here?
+            print("Early return")
+            return False, []
+
+        next_edges = self.grid.get_neightbour_edges(edge)
+        print(f"next edges: {next_edges}")
+        for ne in next_edges:
+            if self.grid.edge_feaisable(ne):
+                ne_dist = dist + 1
+                print(f"next_edge {ne} was feasialbe with dist: {ne_dist}")
+
+                if ne_dist < self.distances[ne]:
+                    print(f"next edge {ne} was smaller than saved dist.\n {self.distances[ne]}")
+                    self.distances[ne] = ne_dist
+                    # Current edge becomes parent to next_edge
+                    self.parents_[self.calcualte_pos_id(ne)] = edge
+                    heapq.heappush(self.queue_, (ne_dist, ne))
+
+        return False, []
+
+    def reconstructShortestPath(self, end_edge) -> list[tuple]:
+        loop_edge = end_edge
+        shortestPath = []
+        print(self.parents_)
+        # print("Reversing thorugh parent tree to find the path")
+        while loop_edge != (-1, -1):
+            print(f"{loop_edge}, ", end="")
+            # print(f"Appending edge {loop_edge}, with id: {self.calcualte_pos_id(loop_edge)}")
+            shortestPath.append(loop_edge)
+            # print(f"next loop_edge is {self.parents_[self.calcualte_pos_id(loop_edge)]} ")
+            loop_edge = self.parents_[self.calcualte_pos_id(loop_edge)]
+            # time.sleep(1)
+        return shortestPath[::-1]
+
+
+
+if __name__ == '__main__':
+    # pass
+    grid = Grid2D.get_test_grid()
+    algo = Dikstras(grid, (9,8), (14,15))
+    path_found = False
+    while not path_found:
+        path_found, path = algo.iter()
+    # print(path)
+    print(f"Distances: {algo.distances}")
+
+    fig, ax = plt.subplots()
+    
+    x = [e[0] for e in path]
+    y = [e[1] for e in path]
+    algo.grid.plot_grid(ax)
+    ax.scatter(x, y, zorder=2)
+    ax.plot(x,y)
+    plt.show()
+    
+
+
---
+++ grid.py
@@ -0,0 +1,166 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from numpy._core.defchararray import lstrip
+# from abc import ABC, abstractmethod
+
+class Grid2D:
+
+    def __init__(self, grid: np.ndarray) -> None:
+        """ 2D grid"""
+        self.grid: np.ndarray = grid
+        # self.feasable_edges = np.arange(1, grid.shape[1])
+        self.xEdgeRange: np.ndarray = np.arange(0,grid.shape[0]+1, 1)
+        self.yEdgeRange: np.ndarray = np.arange(0, grid.shape[1]+1, 1)
+
+    @classmethod
+    def get_test_grid(cls) -> "Grid2D":
+        grid = np.zeros((16,16))
+        grid[10, 8:12] = 1
+        grid[5:10, 11] = 1
+        # grid[0,:] = 1
+        # grid[1,3] = 1
+        return cls(grid)
+    
+    def set_cell(self, cell: tuple[int,int], value:int = 1):
+        """Sets a cell in the grid to value.
+            @param cell: tuple[row, column]"""
+        self.grid[cell] = value
+
+    def get_edges(self, cell: tuple[int,int]) -> list[tuple]:
+        """Returns all edges connected to a grid cell
+            @param cell: (row,colum)
+            @returns edge = (x, y)
+        """
+        row, column = cell
+        if row < 0 or column < 0:
+            print(f"Grid cell {row, column} out of bounds.")
+            return []
+        if row > self.xEdgeRange.max() or column > self.yEdgeRange.max():
+            print(f"Grid cell {row, column} out of bounds.")
+            return []
+
+        edges = [
+            (column, row,),    # Bottom left
+            (column, row+1,),   # top left
+            (column+1, row+1,), # top right
+            (column+1, row)    # bottom right
+        ]
+
+        return edges
+
+    def get_grid_cells(self, edge: tuple[int, int]) -> list[tuple]:
+        """Returns all grid cells that are connected to a edge.
+            @returns list[cell] where cell = (row, column)
+            -----------------
+            |   |   |   |   |
+            |   | x | x |   |
+            --------E--------
+            |   | x | x |   |
+            |   |   |   |   |
+            -----------------
+        """
+        column, row = edge
+        adjunct_cells = [
+            (row, column),      # Top right
+            (row-1, column),    # Bottom right
+            (row-1,column-1),   # Bottom left
+            (row, column-1)     # Top left
+        ]
+        cells_in_bounds = []
+        for cell in adjunct_cells:
+            r, c = cell
+            if r < 0 or c < 0:
+                continue
+            if r >= self.grid.shape[0] or c >= self.grid.shape[1]:
+                continue
+            cells_in_bounds.append(cell)
+
+        return cells_in_bounds
+    
+
+    def get_neightbour_edges(self, edge: tuple[int, int]) -> list[tuple]:
+        """Returns all feasialbe neighbour edges to a edge"""
+
+        connected_cells = self.get_grid_cells(edge)
+
+        adjunct_edges = []
+        for c in connected_cells:
+            adjunct_edges.extend(self.get_edges(c))
+
+        # Remove duplicates
+        adjunct_edges = list(set(adjunct_edges))
+        # Remove current edge
+        adjunct_edges.remove(edge)
+        return adjunct_edges
+
+    def edge_feaisable(self, edge: tuple[int,int]) -> bool:
+        connected_cells = self.get_grid_cells(edge)
+        for c in connected_cells:
+            if self.grid[c] != 0:
+                return False
+        return True
+ 
+    def plot_grid(self, ax: Axes) -> None:
+        ax.pcolormesh(self.xEdgeRange, self.yEdgeRange, self.grid, cmap="Greys", zorder=0)
+        ax.set_xticks(self.xEdgeRange)
+        ax.set_yticks(self.yEdgeRange)
+        ax.grid(True,zorder=1)
+        # ax.scatter(self.edgesX, self.edgesY)
+
+
+def test_grid(test_cell, test_edge):
+
+    fig, ax = plt.subplots()
+    grid = np.zeros((16,16))
+    g = Grid2D(grid)
+
+    # grid_c: row, column
+    grid_c = test_cell
+    g.grid[grid_c] = 1
+
+    edges = g.get_edges(*grid_c)
+    x = [e[0] for e in edges]
+    y = [e[1] for e in edges]
+    ax.scatter(x, y, zorder=2, color="red")
+
+    edge = test_edge
+    ax.scatter(*edge, zorder=2)
+    grid_cells = g.get_grid_cells(edge)
+    for cell in grid_cells:
+        g.grid[cell] = 1
+
+
+    # edge = (1,5)
+    # ax.scatter(edge[0], edge[1])
+
+
+    # edge = (5, 1)
+    # ax.scatter(*edge)
+    # cells = g.get_grid_cell(edge)
+    # for c in cells:
+    #     g.grid[c]=1
+
+    g.plot_grid(ax)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # test_grid(test_cell=(2,6), test_edge=(16,16))
+    fig, ax = plt.subplots()
+    g = Grid2D.get_test_grid()
+
+    edge = (15,15)
+    neighbour_edges = g.get_neightbour_edges(edge)
+    x = [ e[0] for e in neighbour_edges ]
+    y = [ e[1] for e in neighbour_edges ]
+    ax.scatter(x,y,color="red", zorder=2)
+
+
+    ax.scatter(*edge, zorder=3)
+
+    g.plot_grid(ax)
+    plt.show()
+    
+
